de751152fc01b002496a2508cf5d97bb49f0625f
AniruddhaBhaumik <bhaumikaniruddha2003@gmail.com>
2023-10-19 21:22:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DummyData from "./Components/DummyData";
+
+function App() {
+  return (
+    <div className="App">
+      <DummyData />
+    </div>
+  );
+}
+
+export default App;
